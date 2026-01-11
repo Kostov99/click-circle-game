ddb9fb8edf5a78405a8fb1dc21c45a73f9f4750f
Kostov Phuk <moinaphukon2@gmail.com>
2026-01-11 11:42:09 +0530

    Add Hinglish stories data to data-hinglish.js

---
+++ data-hinglish.js
@@ -0,0 +1,12 @@
+const stories_hinglish = [
+  {
+    title: "Alarm Ka Dhokha",
+    content: "Subah alarm baja. Sapna toota. Zindagi shuru.",
+    time: "30 sec"
+  },
+  {
+    title: "Mobile Ka Sach",
+    content: "Maa boli mobile kam chalao. Google khol diya.",
+    time: "20 sec"
+  }
+];
