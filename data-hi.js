0507aa63b5f812508aa86d13fc52b8c1bfe98bb3
Kostov Phuk <moinaphukon2@gmail.com>
2026-01-11 11:43:39 +0530

    Add Hindi stories data to data-hi.js

---
+++ data-hi.js
@@ -0,0 +1,12 @@
+const stories_hi = [
+  {
+    title: "Alarm Ka Dhokha",
+    content: "सुबह अलार्म बजा। सपना टूटा। असली ज़िंदगी चालू।",
+    time: "30 sec"
+  },
+  {
+    title: "Mobile Ka Sach",
+    content: "माँ बोली मोबाइल कम चलाओ। मैंने गूगल खोल लिया।",
+    time: "20 sec"
+  }
+];
