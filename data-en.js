f8c305d7fd412c9849535a0a36de6abe7d5e5644
Kostov Phuk <moinaphukon2@gmail.com>
2026-01-11 11:39:19 +0530

    Add English stories to data-en.js

---
+++ data-en.js
@@ -0,0 +1,12 @@
+const stories_en = [
+  {
+    title: "Alarm Lie",
+    content: "The alarm rang. The dream broke. Reality started.",
+    time: "30 sec"
+  },
+  {
+    title: "Mobile Truth",
+    content: "Mom said stop using phone. I googled how.",
+    time: "20 sec"
+  }
+];
