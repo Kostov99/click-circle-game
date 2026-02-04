73e646a7e767110369550040599be1afce76ea53
Kostov Phuk <moinaphukon2@gmail.com>
2026-02-04 19:47:55 +0530

    Update data-hi.js

--- data-hi.js
+++ data-hi.js
@@ -8,5 +8,11 @@ const stories_hi = [
     title: "Mobile Ka Sach",
     content: "माँ बोली मोबाइल कम चलाओ। मैंने गूगल खोल लिया।",
     time: "20 sec"
-  }
+  },
+  {
+    title:"Kutta",
+    content: "gdhhdjdjjd.    ejjejhfffggfffffffffffggffggtfffgggggggggggggggggggtg
+      hhj
+      uuuu": "20 sec"
+}
 ];
